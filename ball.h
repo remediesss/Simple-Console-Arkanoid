4a2dbc43340e7edfa2c3448fcd5b18a3ffe6a235
remediesss <167404465+remediesss@users.noreply.github.com>
2024-06-28 14:49:33 +0300

    Update ball.h

--- ball.h
+++ ball.h
@@ -3,7 +3,6 @@
 
 #include <math.h>
 
-// Declare the structure of the ball here if it's not declared in another common header
 typedef struct {
 	double x, y;
 	int ix, iy;
@@ -11,7 +10,6 @@ typedef struct {
 	double speed;
 } TBall;
 
-// Function declarations
 void putBall();
 void moveBall(double x, double y);
 void initBall();
